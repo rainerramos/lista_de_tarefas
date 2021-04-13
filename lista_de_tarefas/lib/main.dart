0673cae9e1c5fd9bd7a6cbb95002fae7ff2ad863
Rainer Ramos <rainermr89@hotmail.com>
2021-04-13 15:31:14 -0300

    Alteração RaisedButton para ElevatedButton e criação da lista

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -19,7 +19,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _toDoList = [];
+  List _toDoList = ["Rainer","Marcos"];
 
   @override
   Widget build(BuildContext context) {
@@ -41,17 +41,30 @@ class _HomeState extends State<Home> {
                         labelText: "Nova Tarefa",
                         labelStyle: TextStyle(color: Colors.blueAccent),
                       ),
-                    ),
+                    )
                 ),
-                TextButton(
+                ElevatedButton(
                   style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
-                  ),
-                  onPressed: () { },
+                    backgroundColor: MaterialStateProperty.resolveWith((states) =>  Colors.blueAccent)),
                   child: Text("ADD"),
-                )
+                  onPressed: (){},
+                ),
               ],
             ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top:10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]), 
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(_toDoList[index]["ok"] ?
+                          Icons.check : Icons.error),),
+                  );
+                }),
           )
         ],
       ),
